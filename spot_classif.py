7e30d6c4f80241c8af3ed08fad7071c922b87f62
Porfirio Quintero <porfirioqc@gmail.com>
2017-12-14 15:35:15 -0800

    classif pp7 spots; slightly tidier

--- spot_classif.py
+++ spot_classif.py
@@ -1,5 +1,8 @@
-from matplotlib import pyplot as plt import pandas as pd
+from matplotlib import pyplot as plt 
+from im_utils import *
+import pandas as pd
 import numpy as np
+from tqdm import tqdm
 import seaborn as sns
 from sklearn.decomposition import PCA
 from sklearn.manifold import TSNE
@@ -7,34 +10,21 @@ import sklearn
 import pickle
 
 """
-Attempt to Classify spots using a machine learning
+Classify spots using SVMs
 """
 
-peaks = pd.read_csv('../output/111617_TL47PP7smFish.csv')
-# get 10by10 box containing candidate spots
-def get_bbox(center, im, size=10, return_im=True, pad=2):
-    """ get 10 by 10 bounding box from center"""
-    x, y = center
-    s = int(size/2)
-    x, y = int(x), int(y)
-    bbox = np.s_[y-s:y+s, x-s:x+s]
-    im_bbox = im[bbox]
-    if return_im:
-        if pad:
-            im_bbox = np.pad(im_bbox, pad, 'constant', constant_values=0)
-        return im_bbox
-    else: return bbox
+# load z-projected movies
+ims = {}
+ddir = '../data/FISH/111617_TL47WT_GAL10PP7Cy5_100%int300ms02uZslice/'
+for fname in tqdm(os.listdir(ddir)):
+    if 'tif' in fname:
+        ims[fname.split('.')[0]] = io.imread(ddir + fname)
+    else: next
 
-
-peaks['uid'] = np.arange(len(peaks))
-# get only peaks not in TS using unique id
-TS_certain = peaks.sort_values('signal', ascending=False).groupby('cellid').apply(lambda x: x.iloc[0])
-peaks_woTS = peaks[~(peaks.uid.isin(TS_certain.uid.values))]
-
-# smooth images and substract gaussian background
+# get smFISH image, substract gaussian background and smooth
 ims_smooth = {}
 for imname in ims:
-    fish_im = ims[imname][:,:,0]
+    fish_im = ims[imname][:,:,0].copy()
     # Convert image to float
     fish_im = normalize_im(fish_im)
     # substract background with strong gaussian blur
@@ -45,92 +35,23 @@ for imname in ims:
     ims_smooth[imname] = fish_im
 peaks_woTS = peaks_woTS.apply(lambda x: [normalize_im(get_bbox(x[['x','y']],
                             ims_smooth[x.imname]))], axis=1)
-# take a look at spots
-#peaks_woTS  = np.stack([i[0] for i in peaks_woTS])# if np.array_equal(i[0].shape, (10,10))])
-fig, ax = plt.subplots(1)
-peaks_concat = concat_movies(peaks_woTS, nrows=50)[0]
-plt.imshow(peaks_concat, cmap='viridis')
 
-def sel_training(peaks_df, ims_dict, s=10, nrows=50, cmap='viridis'):
-    """
-    Manual click-selection of training set.
-    Use a large screen if number of candidate objects is large!
-
-    Arguments
-    ---------
-    peaks: dataframe
-        df with object coordinates and corresponding image name
-        Must contain columns ['x','y','imname']
-    ims_dict: dictionary of images
-    s: int
-        size of window to get from image around object coordinates
-    nrows: int
-        number of rows of array to display object images
-
-    Returns
-    ---------
-    sel_bool: boolean array
-        Can be used to index original `peaks` dataframe. True for selected ims.
-    all_ims: array
-        Screened objects. To get selected, index as ims[sel_bool]
-
-    """
-    def check_borders(coords, im, s):
-        """
-        Check if coords are closer than s pixels to borders of im
-        Return False if too close, convenient for indexing
-        """
-        dimx, dimy = im.shape
-        return (coords.x>s)&(coords.x+s<dimx)&(coords.y>s)&(coords.y+s<dimy)
-
-    # create id first keep track of original rows
-    peaks = peaks_df.copy()
-    # clear peaks too close to image border
-    not_inborder = peaks.apply(lambda x: check_borders(x[['x','y']],
-                                            ims_dict[x.imname], s), axis=1)
-    peaks = peaks.loc[not_inborder]
-    peaks['uid'] = np.arange(len(peaks))
-    # get s by s squares containing spots
-    peaks_ims = peaks.apply(lambda x: [normalize_im(get_bbox(x[['x','y']],
-                            ims_dict[x.imname], s))], axis=1)
-    # append extra frames if necessary to make square array with nrows
-    extra_frames, im_shape = len(peaks_ims)%nrows, peaks_ims.iloc[0][0].shape
-    if extra_frames > 0:
-        add_frames = nrows-extra_frames
-        peaks_ims = peaks_ims.append(pd.Series([[np.zeros(im_shape)]\
-                                for f in range(add_frames)]))
-    # concatenate squares for selection
-    peaks_imsconcat = concat_movies(peaks_ims, nrows=nrows)[0]
-    # create s by s squares with labels to track selection and concatenate
-    labels = [[np.full(im_shape, l)] for l in range(len(peaks_ims))]
-    labels = concat_movies(labels, nrows=nrows)[0]
-    # display for click selection
-    fig, ax = plt.subplots(1, figsize=(25.6, 13.6))
-    ax.set_title('click to select; ctrl+click to undo last click; alt+click to finish')
-    ax.imshow(peaks_imsconcat, cmap=cmap)# array of frames for visual sel
-    ax.imshow(labels, alpha=0.0)# overlay array of squares with invisible labels
-    # yticks for guidance, take into account padding
-    ax.set_yticks(np.arange(s+4, nrows*1.1*(s+4), 10))
-    plt.tight_layout()
-    # get labels by click
-    coords = plt.ginput(10000, timeout=0, show_clicks=True)
-    plt.close('all')
-    if len(coords)>0:
-        # filter selected labels
-        selected = {labels[int(c1), int(c2)] for (c2, c1) in coords}
-    else: selected = []
-    # get boolean array of selected for indexing original df
-    sel_bool = peaks.uid.isin(selected).values
-    # get selected images, without padding nor normalizing. Need to fetch originals again
-    peaks_ims = peaks.apply(lambda x: [get_bbox(x[['x','y']],
-                    ims_dict[x.imname], s, pad=False)], axis=1)
-    all_ims  = np.stack([i[0] for i in peaks_ims])
-    return sel_bool, all_ims
-# do manual selection of training set from pre-selected candidate peaks
 peaks = pd.read_csv('../output/111617_TL47PP7smFish.csv')
-#sel_ind, ims_training = sel_training(peaks, ims_smooth, nrows=50)
 
 peaks = pd.read_csv('../output/spot_trainingset/111617_TL47PP7smFish_SpotManualSel.csv')
+# clear spots too close to image borders
+not_inborder = peaks.apply(lambda x: check_borders(x[['x','y']],
+                                        ims_smooth[x.imname], 10), axis=1)
+peaks = peaks[not_inborder]
+# manual selection of training set from pre-selected candidate peaks
+mrna_sorted = peaks[peaks.manual_sel=='crap'].sort_values('signal')
+sel_ind, ims_training = sel_training(mrna_sorted, ims_smooth, nrows=32, scale=0.5, s=25, cmap='gist_stern')
+
+# get only peaks not in TS using unique id
+TS_certain = peaks.sort_values('signal', ascending=False).groupby('cellid').apply(lambda x: x.iloc[0])
+peaks_woTS = peaks[~(peaks.uid.isin(TS_certain.uid.values))]
+
+
 peaks['uid'] = np.arange(len(peaks))
 peaks_ts = peaks[(peaks.manual_sel==1)&(peaks.signal>120)]
 sel_ind_ts, ims_training_ts = sel_training(peaks_ts, ims_smooth, s=25, nrows=10, cmap='gist_stern')
@@ -139,10 +60,7 @@ peaks.loc[peaks.uid.isin(peaks_ts[peaks_ts.TS==1].uid), 'manual_sel'] = 'TS'
 peaks.loc[peaks.manual_sel==1, 'manual_sel'] = 'mrna'
 peaks.loc[peaks.manual_sel==0, 'manual_sel'] = 'crap'
 
-not_inborder = peaks.apply(lambda x: check_borders(x[['x','y']],
-                                        ims_smooth[x.imname], 10), axis=1)
-peaks = peaks[not_inborder]
-peaks_r = peaks[peaks.manual_sel=='crap']
+peaks_r = peaks[peaks.manual_sel=='crap'].sort_values('mass')
 
 ind_r = []
 ims_r = []
@@ -154,7 +72,6 @@ for n in np.arange(step, len(peaks_r), step):
         ind_, ims_= sel_training(peaks_r[n-step:n], ims_smooth, s=10, nrows=15, cmap='viridis')
     ind_r.append(ind_)
     ims_r.append(ims_)
-
 
 ind_, ims_= sel_training(peaks_r[0:step], ims_smooth, s=5, nrows=15, cmap='gist_stern')
 
@@ -176,14 +93,27 @@ with open('../output/spot_trainingset/lauren_sel_mrna.pkl', 'wb') as f:
 io.imshow(im_block(ims_lauren_all[ind_lauren_all], 20, norm=1), cmap='viridis')
 io.imshow(im_block(ims_lauren_all[~ind_lauren_all], 50, norm=1), cmap='viridis')
 
-io.imshow(im_block(ims_training[peaks.manual_sel=='mrna'], 50, norm=1), cmap='gray')
+io.imshow(im_block(ims_training[peaks.manual_sel=='mrna'], 50, norm=1), cmap='viridis')
 plt.figure()
-io.imshow(im_block(ims_training[(peaks.manual_sel=='crap')], 75, norm=1), cmap='viridis')
-io.imshow(im_block(ims_training[spot_labels=='mrna'], 75, norm=1), cmap='viridis')
+io.imshow(im_block(ims_training[(peaks.manual_sel=='TS')], 15, norm=1), cmap='viridis')
 
-io.imshow(im_block(ims_training[peaks[peaks.sort_values('nuc_label').manual_sel=='crap'].index], 75, norm=1), cmap='gray')
-io.imshow(im_block(ims_training[peaks[peaks.sort_values('raw_mass').manual_sel=='mrna'].index], 50, norm=0), cmap='viridis')
+fig, axes = plt.subplots(2, sharex=True, sharey=True)
+axes[0].imshow(im_block(ims_training, 75, norm=1), cmap='viridis')
+axes[1].imshow(np.clip(im_block(ims_training, 75, norm=0), 0, 0.05), cmap='viridis')
 
+io.imshow(np.clip(im_block(ims_training[peaks.manual_sel=='mrna'], 25, norm=0), 0, 0.025), cmap='viridis')
+plt.figure()
+io.imshow(np.clip(im_block(ims_training[peaks.manual_sel=='TS'], 15, norm=0), 0, 0.85), cmap='viridis')
+
+io.imshow(np.clip(im_block(spot_ims[spot_labels=='mrna'], 50, norm=0), 0, 0.025), cmap='viridis')
+
+io.imshow(np.clip(im_block(spot_ims[spot_labels=='crap'], 75, norm=0), 0, 0.025), cmap='viridis')
+io.imshow(np.clip(im_block(spot_ims[spot_labels=='mrna'], 50, norm=0), 0, 0.025), cmap='viridis')
+io.imshow(np.clip(im_block(spot_ims[spot_labels=='TS'], 15, norm=0), 0, 0.025), cmap='viridis')
+plt.figure()
+io.imshow(im_block(spot_ims[spot_labels=='mrna'], 50, norm=1), cmap='viridis')
+plt.figure()
+io.imshow(im_block(spot_ims[spot_labels=='TS'], 15, norm=1), cmap='viridis')
 
 
 peaks.loc[peaks.uid.isin(crap[sel_ind_r].uid), 'manual_sel'] = 'mrna'
@@ -240,7 +170,7 @@ spot_ims = np.stack([np.ravel(i) for i in spot_ims])
 #peaks.drop(889, inplace=True)
 #spot_ims = sklearn.preprocessing.StandardScaler().fit_transform(spot_ims)
 # 10% of dimensions, height and width
-n_components, h, w = 10, 10, 10
+n_components, h, w = 10, 9, 9
 # apply dimensionality reduction
 pca = PCA(n_components=n_components, svd_solver='randomized',
           whiten=True).fit(spot_ims)
@@ -298,11 +228,10 @@ spot_ims = np.stack([np.ravel(i) for i in spot_ims])
 spot_train, spot_test, labels_train, labels_test = \
     model_selection.train_test_split(spot_ims, spot_labels, test_size=0.25,
                                      random_state=42)
-
 #SVM with an RBF kernel, which takes two parameters, C
 # Find best pair of parameters and train
-C_range = np.logspace(-1, 5, 4)
-gamma_range = np.logspace(-3, 0, 4)
+C_range = np.logspace(-6, 1, 10)
+gamma_range = np.logspace(-6, 1, 10)
 param_grid = dict(gamma=gamma_range, C=C_range)
 clf = model_selection.GridSearchCV(SVC(kernel='rbf', class_weight='balanced'),
                                    param_grid)
@@ -344,7 +273,7 @@ label_true_miscl = labels_test[miscl]
 label_pred_miscl = labels_pred[miscl]
 labels_cl = ['True:{0}'.format(str(l1),str(l2)) for (l1, l2) in zip(label_true_miscl, label_pred_miscl)]
 plt.figure()
-plot_gallery(spot_miscl, labels_cl, 10, 10, 8, 16)
+plot_gallery(spot_miscl, 1, 4, reshape=(9,9), titles=labels_cl)
 
 # get indices to randomly look at our predictions
 rows, cols = 10, 29
@@ -355,30 +284,56 @@ s_photos = spot_test[r_ind]
 labels_pred_sample = labels_pred[r_ind]
 
 # plot predictions
-plot_gallery(s_photos, labels_pred_sample, 10, 10, n_row=rows, n_col=cols, fig_title="Predictions")
-def plot_gallery(images, titles, h, w, n_row=3, n_col=4, fig_title=None):
-    """
-    Helper function to plot a gallery of portraits
-    """
-    fig = plt.figure(figsize=(1.8 * n_col, 2.4 * n_row))
-    plt.subplots_adjust(bottom=0, left=.01, right=.99, top=.90, hspace=.35)
-    for i in range(n_row * n_col):
-        ax = fig.add_subplot(n_row, n_col, i + 1)
-        ax.imshow(images[i].reshape((h, w)), cmap=plt.cm.gray)
-        ax.set_title(titles[i], size=12)
-        plt.xticks(())
-        plt.yticks(())
-    if fig_title: fig.suptitle(fig_title+'\n', fontsize=20)
+plot_gallery(s_photos, 10, 10, titles=labels_pred_sample, n_row=rows, n_col=cols, fig_title="Predictions")
 
-def im_block(ims, cols, norm=True):
-    if norm: 
-        ims = normalize(ims)
-    nrows = int(ims.shape[0]/cols)
-    xdim, ydim = ims.shape[1:]
-    block = []
-    for c in np.arange(0, cols*nrows, cols):
-        block.append(np.hstack(ims[c:c+cols]))
-    block = np.vstack(block)
-    return block
+# Classify PP7 spots ========================================================
 
+pp7 = pd.read_pickle('../output/pp7/nuclei_peaks.p')
+# get a sample of brightest spots per trajectory
+#pp7['pid'] = pp7.apply(lambda x: str(x.particle)+x.imname, axis=1)
+#pp7_brightest = pp7.sort_values('mass').drop_duplicates('pid', keep='last')
+#pp7_sample = pp7_brightest.sample(500, random_state=42)
+#not_inborder = pp7_sample.apply(lambda x: check_borders(x[['x','y']],
+#                                            movs[x.imname][x.frame], s), axis=1)
+#pp7_sample = pp7_sample.loc[not_inborder]
+## found spots with low minmass (stored in _parts) to use as crap class for SVM
+#crap = _parts[_parts<500].sample(1000, random_state=42)
+## label images of sample
+#spot_labels, spot_ims = sel_training(crap, movs, movie=True,
+#        scale=1, ncols=50, mark_center=False, s=9, normall=0)
+#io.imshow(im_block(spot_ims[spot_labels], 3, norm=1), cmap='viridis')
+#plt.figure()
+#io.imshow(im_block(spot_ims[~spot_labels], 20, norm=1), cmap='viridis')
+#real_parts_pid = pp7_sample[spot_labels].pid.values
+#real_parts = pp7[pp7.pid.isin(real_parts_pid)]
+#sel = _spot_ims[:150]
+#sel_crap = spot_ims[~spot_labels]
+#io.imshow(im_block(sel_crap, 20, norm=1), cmap='viridis')
+#spot_ims = np.concatenate((sel, sel_crap))
+#spot_labels = np.concatenate((np.full(len(sel), 1), np.full(len(sel_crap),0)))
+##shuffle
+#ind = np.arange(0, len(spot_ims))
+#np.random.shuffle(ind)
+#spot_ims, spot_labels = spot_ims[ind], spot_labels[ind]
+#spot_labels = spot_labels>0
+#spot_ims = normalize(spot_ims)
+#io.imshow(im_block(spot_ims[spot_labels>0], 20, norm=0), cmap='viridis')
 
+# train classifier, using code above in the meantime
+# save classifier
+with open('../output/pp7/clf_beta0_pp7spots.pkl', 'wb') as f:
+    pickle.dump(clf, f)
+
+# Classify PP7 images ========================================================
+# load classifier
+with open('../output/pp7/clf_beta0_pp7spots.pkl', 'rb') as f:
+    clf = pickle.load(f)
+# classify!
+pp7_clf = classify_spots_from_df(pp7, clf, movs_smooth, 10, movie=True)
+# Need to normalize: it's structure, not intensity that matters
+pp7_labels_pred, pp7_ims = classify_spots_from_df(pp7, clf, movs, 9,
+        movie=True, norm=True)
+# check them out
+plt.imshow(im_block(pp7_ims[pp7_labels_pred.plabel==True], 100), cmap='gist_stern')
+# seems alright, just get those trajectories with at least two good spots
+svm_good_parts = filter_parts(pp7_labels_pred, thresh=2)
