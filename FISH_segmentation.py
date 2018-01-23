abddb0be62b51971993a37109d017f61bac10c72
Porfirio Quintero <porfirioqc@gmail.com>
2018-01-23 13:25:57 -0800

    use general segment_fromseeds func; tp.locate df instead of measureprops centers

--- FISH_segmentation.py
+++ FISH_segmentation.py
@@ -23,40 +23,34 @@ for imname in tqdm(ims_projected_dapi):
     dapi = im[:,:,2] # Dapi/nuclear channel
 
     # segment nuclei, channel with best signal ================================
-    print('making nuclei mask with adaptive threshold...')
-    mask_nuclei = mask_image(dapi, min_size=100, block_size=101,
-                selem=skimage.morphology.disk(10))
-    nuclei_markers = skimage.measure.label(mask_nuclei)
     print('finding nuclei centers...') # with peak finding algorithm
     nuc_centers = tp.locate(dapi, diameter=15, minmass=np.median(dapi)*50,
             separation=15)
     # convert center coordinates to image of nuclei seeds for watershed
-    nuc_seeds = np.full_like(dapi, 0)
+    nuc_seeds = np.full_like(dapi, 0) # zero filled array
     for i, (_, r) in enumerate(nuc_centers.iterrows(), 1):
-        nuc_seeds[int(r.y), int(r.x)] = i
-    print('watershed reconstruction from centers...')
-    # remove seeds not in nuclei mask and reconstruct with watershed
-    nuc_seeds = nuc_seeds * mask_nuclei
-    nuclei_markers = skimage.morphology.watershed(nuclei_markers,
-                                    nuc_seeds, mask=mask_nuclei)
+        nuc_seeds[int(r.y), int(r.x)] = i # add labeled centers to array
 
-    # segment cells from nuclei markers =======================================
-    cell_markers, nuclei_markers, mask_cells, mask_nuclei =\
-                        segment_cellfromnuc(autof, nuclei_markers)
+    print('nuclei watershed reconstruction from centers...')
+    nuclei_markers, center_markers = segment_from_seeds(dapi, nuc_seeds,
+            (100, 101, 10), dilate=False)
+
+    print('cell watershed reconstruction from nuclei...')
+    cell_markers, nuclei_markers = segment_from_seeds(autof, nuclei_markers,
+            (1000, 151, 15), dilate=True)
+
     markers_dict[imname] = cell_markers, nuclei_markers
     # draw segmented nuclei and cell boundaries on merged image
     ims_seg[imname] = make_seg_im((cell_markers, nuclei_markers),
         dapi + autof*3 + fish) # enhanced autof because its dimmer than others
 
-    # make nuclei centers dataframe ===========================================
-    # get centroids (so all bbox are same size)
-    nuc_regionprops = skimage.measure.regionprops(nuclei_markers, dapi)
-    _seg_coords = regionprops2df(nuc_regionprops, props=('label', 'centroid'))
+    # update nuclei centers dataframe ===========================================
+    nuc_centers['label'] = nuc_centers.apply(lambda coords:\
+            nuclei_markers[int(coords.y), int(coords.x)], axis=1)
+    _seg_coords = nuc_centers[(nuc_centers.label>0)].reset_index(drop=True)
     # add image name for sel_training func
     _seg_coords['imname'] = imname
     seg_coords = pd.concat((seg_coords, _seg_coords), ignore_index=True)
-# expand centroid into x, y coords
-seg_coords[['y','x']] = seg_coords.centroid.apply(pd.Series)
 # add cell id
 seg_coords['cid'] = seg_coords.apply(lambda x: x.imname+'_'+str(x.label), axis=1)
 
