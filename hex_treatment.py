92c1d1f7c8a9d8b5e7621c1f2cb48e29024de426
Porfirio Quintero <porfirioqc@gmail.com>
2018-05-20 15:14:20 -0700

    func for kymograph

--- hex_treatment.py
+++ hex_treatment.py
@@ -14,7 +14,6 @@ hex92 = io.imread('/Users/porfirio/Documents/research/sternberg_lab/yeastEP/ms2p
 hex92_2 = io.imread('/Users/porfirio/Documents/research/sternberg_lab/yeastEP/ms2pp7/data/HexanediolTreatment/MAX_TL47tl092_40minGalInduced_HexTreatment_2.tif')
 hex92 = np.concatenate((hex92, hex92_2))
 prehex92 = io.imread('/Users/porfirio/Documents/research/sternberg_lab/yeastEP/ms2pp7/data/HexanediolTreatment/MAX_TL47tl092_20minGalInduced_PreHexTreatment.tif')
-
 def mean_med(mov):
     fmean = [np.mean(f) for f in mov]
     fmed = [np.median(f) for f in mov]
@@ -53,14 +52,31 @@ ax = plot(hex75[0])
 test_pre2 = np.stack([f[roi] for f in prehex75])
 trace_pre2 = [np.max(f) for f in test_pre2]
 
+def kymograph(mov, ax=None, t=20, vmin=100, vmax=750, ylabel=True, title=None, 
+        xticks=0, log=False):
+    """ Make and plot kymograph of movie """
+    if ax is None:
+        fig, ax = plt.subplots()
+    # max intensity projection along columns; x coord is same in mov as in plot
+    rowmax = pd.DataFrame([np.max(f, axis=0) for f in mov])
+    # convert frames to minutes
+    rowmax.index = [int(i) for i in rowmax.index*t/60]
+    if log:
+        rowmax = np.log(rowmax)
+        vmin, vmax = np.min(rowmax.values), np.max(rowmax.values)
+    sns.heatmap(rowmax, ax=ax, cmap='viridis', xticklabels=xticks,
+        yticklabels=6, vmin=vmin, vmax=vmax)
+    ax.set_yticklabels(ax.get_yticklabels(), rotation=30, fontsize=10)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=60, fontsize=10)
+    if ylabel: ax.set_ylabel('Time (min)')
+    if title: ax.set_title(title)
+    return ax
+
 fig, axes = plt.subplots(2, 2, sharey='row', figsize=(22, 8))
-for i, (m, n) in zip((3,2,1,0), zip((prehex75, hex75, prehex92, 56     rowmax = pd.DataFrame([np.max(f, axis=0) for f in m])
-    # convert frames to seconds
-    rowmax.index = [int(i) for i in rowmax.index*20/60]
+for i, (m, n) in zip((3,2,1,0), zip((prehex75, hex75, prehex92, hex92))):
     ax = axes.flat[i]
-    sns.heatmap(rowmax, ax=ax, cmap='viridis', xticklabels=0,
-        yticklabels=6, vmin=100, vmax=1500 if '75' in n else 75 62     ax.set_title(n)
-    ax.set_yticklabels(ax.get_yticklabels(), rotation=30, fonts 64 axes[0,0].axhline(5, ls='--', alpha=0.4, c='w')
+    kymograph(m, ax, vmax=1500 if '75' in n else 750, ylabel=False, title=n)
+axes[0,0].axhline(5, ls='--', alpha=0.4, c='w')
 axes[1,0].axhline(3, ls='--', alpha=0.4, c='w')
 axes[0,0].set_ylabel('Time (min)')
 axes[1,0].set_ylabel('Time (min)')
@@ -110,3 +126,8 @@ plt.ylabel('Fluor. (a.u.)')
 plt.tight_layout()
 plt.savefig()
 plt.savefig('../output/hexTreatment/topFoci_75hex.png', dpi= 300, bbox_inches='tight')
+
+pqc72 = io.imread('/Users/porfirio/Documents/research/sternberg_lab/yeastEP/ms2pp7/data/72_TL092/MAX_05142018_pQC725_TL092_4hPostInduction.tif')
+pqc72_2 = io.imread('/Users/porfirio/Documents/research/sternberg_lab/yeastEP/ms2pp7/data/72_TL092/MAX_05142018_pQC725_TL092_3hPostInduction.tif')
+kymograph(pqc72, log=True)
+kymograph(pqc72_2, xticks=20, log=True)
