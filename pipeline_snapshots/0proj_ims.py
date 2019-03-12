b38abebc4171b977ea2337ad6a99a708afe61459
Porfirio Quintero Cadena <porfirioqc@gmail.com>
2019-03-11 18:17:34 -0700

    proj snaphot ims

---
+++ pipeline_snapshots/0proj_ims.py
@@ -0,0 +1,10 @@
+import os
+from joblib import Parallel, delayed
+from utils.image import load_zproject_STKimcollection
+
+datadir = '/Volumes/PQdata/data/PP7/032019_PP7SnapShots/'
+savedir = '../data/2019_pp7Snapshots/'
+dirs_toload = [datadir + d + '/' for d in os.listdir(datadir) if 'DS' not in d]
+proj = Parallel(n_jobs=6)(delayed(load_zproject_STKimcollection)
+                (im_dir+'*STK', savedir) for im_dir in tqdm(dirs_toload))
+
